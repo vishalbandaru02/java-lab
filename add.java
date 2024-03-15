ded6e8d3a7ce5fa23835ae73ffae1e937b9cfd83
vishalbandaru02 <160831185+vishalbandaru02@users.noreply.github.com>
2024-03-15 20:34:35 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class add {
+public static void main (String[] args)
+{ Scanner sc=new Scanner(System.in);
+System.out.println("Enter the Values of a and b:");
+int a=sc.nextInt();
+int b=sc.nextInt();
+System.out.println("addition of "+a+"+"+b+" is "+(a+b));
+}
+}
