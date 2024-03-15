b2de08b5a9769ef470a50d98d7b365e033ecbd80
vishalbandaru02 <160831185+vishalbandaru02@users.noreply.github.com>
2024-03-15 20:59:47 +0530

    Add files via upload

---
+++ Numbergen.java
@@ -0,0 +1,14 @@
+class Numbergen
+{
+   public static void main(String[] args)
+  {
+    for(int i=1;i<=100;i++)
+   {
+    System.out.print(i+" ");
+    if(i%10==0)
+   { 
+    System.out.println();
+   }
+   }
+  }
+}
