bd61ba9ce83c99c5829d4f4300364bd919c87c45
vishalbandaru <160831185+vishalbandaru02@users.noreply.github.com>
2024-04-18 15:40:53 +0530

    Add files via upload

---
+++ strbuffer.java
@@ -0,0 +1,8 @@
+class strbuffer{
+   public static void main(String[] args){
+     StringBuffer sb=new StringBuffer("Study");
+     System.out.println(sb);
+     sb.append("tonight");      
+     System.out.println(sb);
+ }
+}
